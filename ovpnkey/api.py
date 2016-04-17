eecfd65be6fb64d7367c215034714914b507ec9e
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-04-17 21:16:31 +0800

    update

--- ovpnkey/api.py
+++ ovpnkey/api.py
@@ -10,11 +10,6 @@ from tornado import web, process
 from . import utils
 
 
-EASY_RSA_ROOT = '/etc/openvpn/easy-rsa/2.0'
-OPENVPN_HOST = 'wcpan.me'
-OPENVPN_PORT = 1194
-
-
 class OpenVPNHandler(web.RequestHandler):
 
     async def post(self):
@@ -29,8 +24,9 @@ class OpenVPNHandler(web.RequestHandler):
             self.set_status(400)
             return
 
+        easy_rsa_path = self.settings['easy_rsa_path']
         with open(os.devnull, 'w') as null:
-            p = process.Subprocess(['./gk.sh', EASY_RSA_ROOT, email, name], stdout=null, stderr=null)
+            p = process.Subprocess(['./gk.sh', easy_rsa_path, email, name], stdout=null, stderr=null)
             exit_code = await p.wait_for_exit()
 
         if exit_code != 0:
@@ -41,10 +37,12 @@ class OpenVPNHandler(web.RequestHandler):
         with tempfile.TemporaryFile() as fout:
             with zipfile.ZipFile(fout, 'w') as zout:
                 with open('./client.conf', 'r') as tpl:
-                    zout.writestr('{0}/client.conf'.format(prefix), tpl.read().format(host=OPENVPN_HOST, port=OPENVPN_PORT, name=name))
-                zout.write('{0}/keys/ca.crt'.format(EASY_RSA_ROOT), '{0}/ca.crt'.format(prefix))
-                zout.write('{0}/keys/{1}.crt'.format(EASY_RSA_ROOT, name), '{0}/{1}.crt'.format(prefix, name))
-                zout.write('{0}/keys/{1}.key'.format(EASY_RSA_ROOT, name), '{0}/{1}.key'.format(prefix, name))
+                    openvpn_host = self.settings['openvpn_host']
+                    openvpn_port = self.settings['openvpn_port']
+                    zout.writestr('{0}/client.conf'.format(prefix), tpl.read().format(host=openvpn_host, port=openvpn_port, name=name))
+                zout.write('{0}/keys/ca.crt'.format(easy_rsa_path), '{0}/ca.crt'.format(prefix))
+                zout.write('{0}/keys/{1}.crt'.format(easy_rsa_path, name), '{0}/{1}.crt'.format(prefix, name))
+                zout.write('{0}/keys/{1}.key'.format(easy_rsa_path, name), '{0}/{1}.key'.format(prefix, name))
             fout.seek(0, 0)
 
             self.set_header('Content-Type', 'application/octet-stream')
--- ovpnkey/utils.py
+++ ovpnkey/utils.py
@@ -1,15 +1,51 @@
 # ex: ts=4 sts=4 sw=4 et
 
+import argparse
 import contextlib
 import grp
 import os
 import pwd
 import re
+import socket
 import sys
 
 from tornado import ioloop, web, httpserver, netutil, log
 
 from . import views, api
+
+
+class TCPEndpoint(object):
+
+    def __init__(self, interface, port):
+        self._interface = interface
+        self._port = port
+
+    def __enter__(self):
+        self._sockets = netutil.bind_sockets(self._port, self._interface, socket.AF_INET)
+        return self._sockets
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        for skt in self._sockets:
+            skt.close()
+
+
+class UNIXEndpoint(object):
+
+    def __init__(self, path):
+        self._path = path
+        self._user = 'www-data'
+        self._group = 'www-data'
+
+    def __enter__(self):
+        uid = pwd.getpwnam(self._user).pw_uid
+        gid = grp.getgrnam(self._group).gr_gid
+        self._socket = netutil.bind_unix_socket(self._path)
+        os.chown(self._path, uid, gid)
+        return [self._socket]
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._socket.close()
+        os.remove(self._path)
 
 
 def check_name(name):
@@ -29,9 +65,68 @@ def unix_socket(path, u, g):
             os.remove(path)
 
 
+@contextlib.contextmanager
+def create_sockets(endpoint_list):
+    with contextlib.ExitStack() as stack:
+        sockets = (stack.enter_context(_) for _ in endpoint_list)
+        sockets = [skt for list_ in sockets for skt in list_]
+        yield sockets
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser('ovpnkey')
+
+    parser.add_argument('-l', '--listen', type=str, action='append')
+    parser.add_argument('-p', '--openvpn-port', nargs='?', type=int, default=1194)
+    parser.add_argument('-e', '--easy-rsa-path', required=True, type=str)
+
+    args = parser.parse_args(args)
+    safe_args = {}
+    safe_args['listen'] = [verify_listen_string(_) for _ in args.listen]
+    safe_args['openvpn_port'] = args.openvpn_port
+    safe_args['easy_rsa_path'] = args.easy_rsa_path
+
+    return safe_args
+
+
+def verify_listen_string(listen):
+    # port only
+    if verify_port(listen):
+        return TCPEndpoint('0.0.0.0', int(listen))
+    # ipv4:port
+    m = listen.split(':', 1)
+    if len(m) == 2 and verify_ipv4(m[0]) and verify_port(m[1]):
+        return TCPEndpoint(m[0], int(m[1]))
+    # path of unix socket
+    return UNIXEndpoint(listen)
+
+
+def verify_ipv4(ipv4):
+    m = re.match(r'^(0|([1-9][0-9]{0,2}))\.(0|([1-9][0-9]{0,2}))\.(0|([1-9][0-9]{0,2}))\.(0|([1-9][0-9]{0,2}))$', ipv4)
+    if m:
+        m = m.groups()
+        m = [m[_] for _ in range(0, len(m), 2)]
+        m = [0 <= int(_) < 256 for _ in m]
+        m = all(m)
+        if m:
+            return True
+    return False
+
+
+def verify_port(port):
+    m = re.match(r'^[1-9]\d{0,4}$', port)
+    if m:
+        m = int(port)
+        if 1 <= m < 65536:
+            return True
+    return False
+
+
 def main(args=None):
     if args is None:
         args = sys.argv
+
+    args = parse_args(args[1:])
 
     log.enable_pretty_logging()
     main_loop = ioloop.IOLoop.instance()
@@ -39,10 +134,10 @@ def main(args=None):
     application = web.Application([
         (r'/', views.IndexHandler),
         (r'/key', api.OpenVPNHandler),
-    ])
+    ], openvpn_port=args['openvpn_port'], easy_rsa_path=args['easy_rsa_path'])
     server = httpserver.HTTPServer(application)
-    with unix_socket('/tmp/ovpnkey.socket', 'www-data', 'www-data') as socket:
-        server.add_socket(socket)
+    with create_sockets(args['listen']) as sockets:
+        server.add_sockets(sockets)
         main_loop.start()
 
     return 0
